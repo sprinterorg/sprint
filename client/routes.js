d53c6852002c87f5f779452bf5fa361a02ea4a4a
artpoddybnyy <artpoddybnyy@gmail.com>
2018-01-27 12:38:57 +0200

    enable html5Mode

--- client/routes.js
+++ client/routes.js
@@ -1,11 +1,11 @@
-import helloTemplate from './app/components/hello-World/helloWorld.component.html';
-import helloCtrl from './app/components/hello-World/helloWorld.controller';
+import helloTemplate from './app/components/hello-world/hello-world.component.html';
+import helloCtrl from './app/components/hello-world/hello-world.controller';
 import hellCtrl from './app/components/hello-hell/hello-hell.controller';
 import helloHellTemplate from './app/components/hello-hell/hello-hell.component.html';
 
 export default /*@ngInject*/ function($locationProvider, $stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
-    // $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
     $stateProvider.state('hello', {
         url: '/hello',
         template: helloTemplate,
