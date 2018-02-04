5e288d81485cc93ea6093c0320e74d8bd8fd3be7
Alex Rubanenko <arubanenko@qualium-systems.com.ua>
2018-02-04 15:07:47 +0200

    my dragula index

--- client/app/components/current-sprint/current-sprint.controller.js
+++ client/app/components/current-sprint/current-sprint.controller.js
@@ -8,22 +8,19 @@ export default class sprintController {
         this._fireBase = fireBase;
         this._scope = $scope;
         this.userId = supportService.getCookie('user');
+        this.isModalOpen = false;
 
         this._scope.$on('second-bag.drag', (e, el) => {
-
-
             el.addClass('ex-moved');
             // var indexInList = Array.prototype.indexOf.call(el.parent().parent()[0].children, el.parent()[0])
         });
 
         this._scope.$on('first-bag.drop', (e, el) => {
             el.removeClass('ex-moved');
-
             var cardId = JSON.parse(el[0].id)
             var newListId = el.parent().parent()[0].children[0].id
             console.log('list', newListId, cardId.$id)
             this._fireBase.moveToList(cardId.$id, Number(newListId) || 1, this.projectId)
-
         });
 
         this._scope.$on('first-bag.over', (e, el, container) => {
@@ -53,5 +50,18 @@ export default class sprintController {
             /*let id = rootRef.key;*/
         });
     };
+    showFullCard(card){
+        console.log(card);
+        this.isModalOpen = true;
+        this.title = card.title;
+        this.cardSuperId = card.$id;
+        this.cardSimpleId = card.id;
+        this.cardListId = card.list_id;
+        this.superPriority = card.$priority;
+        this.priority = card.priority;
+    }
+    closeFullCard(){
+        this.isModalOpen = false;
+    }
 }
 
