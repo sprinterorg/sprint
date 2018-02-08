0be116810ecc0fcf00e3d67e658666a8fb0914e9
Alex <alex.kurilo77@gmail.com>
2018-02-08 18:50:04 +0200

    change profile

--- src/app/components/profile/profile.controller.js
+++ src/app/components/profile/profile.controller.js
@@ -1,3 +1,5 @@
+import './profile.component.scss';
+
 export default class profileController {
     /*@ngInject*/
     constructor(fireBase, supportService, $stateParams) {
@@ -5,17 +7,27 @@ export default class profileController {
         this._fireBase = fireBase;
         this.user = fireBase.getUser(this.userId);
         this.projects = fireBase.getProjects();
+        this.visible = true;
+        this.srcAvatar = supportService.getPicture('spiderman');
+        console.log("constructor = " + this.srcAvatar);
     }
 
     updateUser() {
         let ids = [];
         this.projects.filter( item => Object.keys(item['users']).filter(item => item === this.userId).length>0).map(item => ids.push(item.$id));
         let self = this;
+        console.log("updateUser" + this)
         this._fireBase.updateUser(ids, this.userId, {
         	username: this.user.username,
         	email: this.user.email,
         	avatar: this.user.avatar
         })
+    }
+
+    changeUserInfirmation(){
+        this.visible = !this.visible;
+        console.log(this.visible);
+        console.log("changeAvatar");
     }
 }
 
