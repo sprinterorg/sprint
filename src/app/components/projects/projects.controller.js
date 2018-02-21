cb9d6abcd494a44da53b9480ba82009b0a090f24
Krabonub <temaskok98@gmail.com>
2018-02-21 20:13:58 +0200

    projects form markup + validation (login form markup fix)

--- src/app/components/projects/projects.controller.js
+++ src/app/components/projects/projects.controller.js
@@ -7,24 +7,78 @@ export default class projectsController {
         this.myProjects = fireBase.getMyProjects(this.userId);
         this._fireBase = fireBase;
         this.manager = fireBase.getUser(this.userId);
+
+        this.validateProjectName.bind(this);
+        this.validateDuration.bind(this);
+        this.formValidation.bind(this);
+
+        this.duration = "0";
+        this.projectName = "";
+
+        this.projectNameIsValid = true;
+        this.projectNameLabel = 'project name';
+        this.durationIsValid = true;
+        this.durationLabel = 'duration of the sprint (in days)';
+
+        this.FormIsValid = false;
+    }
+
+    validateProjectName(){
+        if(!this.projectName || this.projectName.length> 50){
+            this.projectNameIsValid = false;
+            this.projectNameLabel = 'incorrect name length';
+            return false;
+        }
+        else{
+            this.projectNameIsValid = true;
+            this.projectNameLabel = 'project name';
+            return true;
+        }
+    }
+
+    validateDuration(){
+        if(!this.duration){
+            this.duration = 0;
+            this.durationIsValid = true;
+            this.durationLabel = 'duration of the sprint (in days)';
+            return true;
+        }
+        if(this.duration <= 0 || isNaN(this.duration)){
+            this.durationIsValid = false;
+            this.durationLabel = 'invalid durration';
+            return false;
+        }   
+        else{
+            this.durationIsValid = true;
+            this.durationLabel = 'duration of the sprint (in days)';
+            return true;
+        }
+    }
+
+    formValidation(){
+        this.FormIsValid = this.validateProjectName() && this.validateDuration();
+        return this.FormIsValid;
     }
 
     addProject () {
-        let self = this;
-        this.hideFunc();
-        this._fireBase.addProject(this.projects, {currentSprint : { 
-            projectName: this.projectName,
-            managerId: this.userId,
-            duration: this.duration,
-            startTimeStamp: Date.now()
-        }}, {
-            username: this.manager.username,
-            email: this.manager.email,
-            avatar: this.manager.avatar
-        }).then( rootRef => {
-            //self.projectName = '';
-            //self.duration = '';
-            self._$state.go('current-sprint', {project_id: rootRef.key});
-        });
+        if(this.formValidation() === true){
+            let self = this;
+            this.hideFunc();
+            this._fireBase.addProject(this.projects, {currentSprint : { 
+                projectName: this.projectName,
+                managerId: this.userId,
+                duration: this.duration,
+                startTimeStamp: Date.now()
+            }}, {
+                username: this.manager.username,
+                email: this.manager.email,
+                avatar: this.manager.avatar
+            }).then( rootRef => {
+                self._$state.go('current-sprint', {project_id: rootRef.key});
+            });
+        }
+        else{
+            return;
+        }
     }
 }
